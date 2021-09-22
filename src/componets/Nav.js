3284064e4cb5f536a16598ad7462b0f345853e1c
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-09-22 08:46:43 +0200

    Dark mode finish 90%

--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -4,30 +4,18 @@ import { faMusic, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = ({ libraryStatus, setLibraryStatus, darkMode, setDarkMode }) => {
 
-    // style
-    const dark = {
-        background:' #000',
-        color: '#fff',
-        borderColor: 'rgb(245, 245, 245)',
-        zIndex: 10,
-    }
-    const light ={
-        background:' #fff',
-        color: '#000',
-        borderColor: 'rgb(65, 65, 65)',
-        zIndex: 10,
-    }
+
     
 
     return (
         <nav>
             <h1>Waves</h1>
-            <div>
-                <button style={darkMode ? dark : light} onClick={() => setLibraryStatus(!libraryStatus)} >
+            <div style={{zIndex: 10}}>
+                <button className={`btn ${darkMode ? 'btn-dark' : 'btn-light'}`} onClick={() => setLibraryStatus(!libraryStatus)} >
                     Library
                     <FontAwesomeIcon icon={faMusic} />
                 </button>
-                <button style={darkMode ? dark : light} onClick={() => setDarkMode(!darkMode)}>
+                <button className={`btn ${darkMode ? 'btn-dark' : 'btn-light'}`} onClick={() => setDarkMode(!darkMode)}>
                     {darkMode ? 'Light' : 'Dark'} 
                     <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
                 </button>
